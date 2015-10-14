872259de74f198e7ebac559c7faf4e2b7360a532
Phuc, Tran Huu <phuc@fiisionstudio.com>
2015-10-14 16:58:23 +0700

    Add MoveFile function.

--- utils.go
+++ utils.go
@@ -1,6 +1,9 @@
 package cocktail
 
 import (
+	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"net/url"
 	"os"
@@ -11,10 +14,30 @@ func CreateDir(path string, perm os.FileMode) {
 	os.MkdirAll(path, perm)
 }
 
+/** Move upload file to a folder. */
+func MoveFile(uploadFile multipart.File, newPath string) error {
+	/* Condition validation */
+	if uploadFile == nil {
+		return fmt.Errorf("Upload file is empty.")
+	}
+	defer uploadFile.Close()
+
+	// Create new file
+	output, err := os.Create(newPath)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	// Move file to new location
+	_, err = io.Copy(output, uploadFile)
+	return err
+}
+
 /** Check if file exist at path or not. */
 func DirExisted(dirPath string) bool {
 	fileInfo, err := os.Stat(dirPath)
-	return fileInfo.IsDir() && err == nil
+	return err == nil && fileInfo.IsDir()
 }
 
 /** Check if file exist at path or not. */
